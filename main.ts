5d3b238141e7b358ae172b510aa400c43c02aae5
Yaroslav Kulyk <ykulik@hotmail.com>
2023-01-27 07:50:25 +0000

    published v1

--- main.ts
+++ main.ts
@@ -1,11 +1,4 @@
 radio.onReceivedValue(function (name, value) {
-    images.createImage(`
-        . . # # .
-        . # # . #
-        . . # # .
-        . # # . #
-        . . # # .
-        `).showImage(0)
     if (name.compare("mgy") == 0) {
         forward = value
         forward = forward * -1
@@ -23,28 +16,25 @@ let forward = 0
 radio.setGroup(1)
 serial.writeLine("Start - wait for radio signal")
 basic.showIcon(IconNames.Square)
+MotorDriver.MotorStop(Motor.A)
+MotorDriver.MotorStop(Motor.B)
 basic.forever(function () {
-    if (left_motor >= 0) {
-        MotorDriver.MotorRun(Motor.B, Dir.forward, left_motor / 100)
+    if (Math.abs(left_motor) < 100 && Math.abs(right_motor) < 100) {
+        basic.showIcon(IconNames.SmallSquare)
+        MotorDriver.MotorStop(Motor.A)
+        MotorDriver.MotorStop(Motor.B)
     } else {
-        MotorDriver.MotorRun(Motor.B, Dir.backward, left_motor / -100)
+        if (left_motor >= 0) {
+            MotorDriver.MotorRun(Motor.B, Dir.forward, left_motor / 100)
+        } else {
+            MotorDriver.MotorRun(Motor.B, Dir.backward, left_motor / -100)
+        }
+        if (right_motor >= 0) {
+            MotorDriver.MotorRun(Motor.A, Dir.forward, right_motor / 100)
+        } else {
+            MotorDriver.MotorRun(Motor.A, Dir.backward, right_motor / -100)
+        }
+        basic.showIcon(IconNames.House)
     }
-    if (right_motor >= 0) {
-        MotorDriver.MotorRun(Motor.A, Dir.forward, right_motor / 100)
-    } else {
-        MotorDriver.MotorRun(Motor.A, Dir.backward, right_motor / -100)
-    }
-})
-loops.everyInterval(5000, function () {
-    serial.writeLine("If no radio received - will stop")
-    basic.showLeds(`
-        # # # # #
-        # . . . #
-        # . . . #
-        # . . . #
-        # # # # #
-        `)
-    serial.writeLine("Motors set to stop")
-    left_motor = 100
-    right_motor = 100
+    basic.pause(200)
 })
